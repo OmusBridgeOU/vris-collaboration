abf741372ce49b5507763938e51de4b4bd19b064
ddesk8910 <yk.deskwork@gmail.com>
2026-05-07 18:19:30 +0900

    TOPページを表示（FirstView, 参加者向け重要情報セクション）

---
+++ layers/main/app/components/ha/HaSectionTitle.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="section-title">
+    <div class="section-title__line" />
+    <p class="section-title__label">
+      {{ label }}
+    </p>
+    <h2 class="section-title__text">
+      {{ title }}
+    </h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/variables' as v;
+
+.section-title {
+  position: relative;
+  margin-bottom: 96px;
+  padding-top: 16px;
+
+  &__line {
+    position: relative;
+
+    overflow: hidden;
+
+    width: 100%;
+    height: 2px;
+    margin-bottom: 2px;
+
+    // 点
+    &::before {
+      content: '';
+
+      position: absolute;
+      top: 0;
+      left: 0;
+
+      width: 2px;
+      height: 2px;
+
+      background: v.$vket-amber;
+    }
+
+    &::after {
+      content: '';
+
+      position: absolute;
+      top: 0;
+      left: 0;
+
+      width: 100%;
+      height: 2px;
+      margin-left: 12px;
+
+      background: linear-gradient(to right, v.$vket-amber 0%, v.$vket-vermilion 100%);
+    }
+  }
+
+  &__label {
+    margin-bottom: 4px;
+    font-size: 12px;
+    color: v.$vket-amber;
+    letter-spacing: 0.1em;
+  }
+
+  &__text {
+    font-size: 32px;
+    font-weight: 700;
+    color: #fff;
+  }
+}
+</style>
---
+++ layers/main/app/components/ht/HtQuickAccessSection.vue
@@ -0,0 +1,56 @@
+<template>
+  <HaSectionTitle
+    title="参加者向け重要情報"
+    label="QUICK ACCESS"
+  />
+  <div class="grid2x">
+    <HaGlassCard
+      class="grid2x__child"
+      variant="link"
+      color="cyan"
+      title="開催日"
+      label="DATE"
+      icon-url="/icons/line-md_calendar.svg"
+    >
+      <template #body>
+        <p>ボディの中身</p>
+      </template>
+    </HaGlassCard>
+    <HaGlassCard
+      class="grid2x__child"
+      variant="link"
+      color="magenta"
+      title="会場"
+      label="LOCATION"
+      icon-url="/icons/mingcute_map-pin-line.svg"
+    >
+      <template #body>
+        <p>ボディの中身</p>
+      </template>
+    </HaGlassCard>
+    <HaGlassCard
+      class="grid2x__child"
+      variant="link"
+      color="amber"
+      title="チケット"
+      label="TICKETS"
+      icon-url="/icons/f7_tickets.svg"
+    >
+      <template #body>
+        <p>ボディの中身</p>
+      </template>
+    </HaGlassCard>
+    <HaGlassCard
+      class="grid2x__child"
+      variant="link"
+      color="vermilion"
+      title="スケジュール"
+      label="SCHEDULE"
+      icon-url="/icons/material-symbols_timer-outline.svg"
+    >
+      <template #body>
+        <p>ボディの中身</p>
+      </template>
+    </HaGlassCard>
+  </div>
+</template>
