084a7f98da44ac896ab8acf294350912d741b998
ddesk8910 <yk.deskwork@gmail.com>
2026-08-27 04:19:06 +0900

    Revert "fix: チケットCTAのガラスデザインを修正"
    This reverts commit c2ae211a1befcacd286a85c594ae2009a5cdc119.

--- layers/main/app/components/ha/HaTicketCard.vue
+++ layers/main/app/components/ha/HaTicketCard.vue
@@ -102,12 +102,6 @@ defineProps<{
     color: white;
     text-decoration: none;
 
-    &::before {
-      inset: 0;
-      width: auto;
-      height: auto;
-    }
-
     &--disabled {
       cursor: not-allowed;
       opacity: 0.68;
